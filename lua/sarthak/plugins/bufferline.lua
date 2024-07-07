784d81cc0f76ed57a446979d599cb3773932e858
Sarthak Narayan <sarthak.narayang@gmail.com>
2024-07-07 09:37:16 -0400

    config

--- lua/sarthak/plugins/bufferline.lua
+++ lua/sarthak/plugins/bufferline.lua
@@ -1,12 +1,14 @@
 -- nice tab view
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      offsets = { { filetype = "NvimTree", text = "File Explorer", padding = 1 } }, -- no tabs over nvim-tree
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+			mode = "tabs",
+			offsets = { { filetype = "NvimTree", text = "File Explorer", padding = 1 } }, -- no tabs over nvim-tree
+		},
+	},
 }
