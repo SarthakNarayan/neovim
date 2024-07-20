05873870662f7c896bd3cba69c5c685e28fa83f4
Sarthak Narayan <sarthak.narayang@gmail.com>
2024-07-19 20:57:45 -0400

    wip

--- vimrc/setup.sh
+++ vimrc/setup.sh
@@ -2,4 +2,5 @@
 
 curl -fsSL https://raw.githubusercontent.com/SarthakNarayan/neovim/main/vimrc/.vimrc > "$HOME/.vimrc"
 mkdir -p ~/.vim/colors
-curl -fsSL 
+curl -fsSL https://raw.githubusercontent.com/SarthakNarayan/neovim/main/vimrc/gruvbox.vim > "$HOME"/.vim/colors/gruvbox.vim
+
